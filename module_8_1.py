6f7c1292c6ad7ad7fc6e4d0dca98b4db6b8baca3
MihailShcherbak <pochta.mihaila@gmail.com>
2025-01-25 02:09:38 +0300

    25.01.2025

--- module_8_1.py
+++ module_8_1.py
@@ -5,7 +5,6 @@ def add_everything_up(a, b):
         res = str(a) + str(b)
     return res
 
-
 print(add_everything_up(123.456, 'строка'))
 print(add_everything_up('яблоко', 4215))
 print(add_everything_up(123.456, 7))
